e772a4ab03b2df96add339605443dd95da943012
AbiramRam <abirammaha22@icloud.com>
2025-06-20 16:19:00 +0530

    rollback

--- src/main/java/com/example/defectTracker/dto/DefectDto.java
+++ src/main/java/com/example/defectTracker/dto/DefectDto.java
@@ -12,12 +12,12 @@ public class DefectDto {
 //    private int reOpenCount;
 //    private Media attachment;
 //    private String steps;
-//    private ReleaseTestCase releaseTestCase;
-//    private User assignedBy;
-//    private User assignedTo;
-//    private Severity severity;
-//    private DefectStatus defectStatus;
-//    private Project project;
-//    private Priority priority;
-//    private Type defectType;
+    private ReleaseTestCase releaseTestCase;
+    private User assignedBy;
+    private User assignedTo;
+    private Severity severity;
+    private DefectStatus defectStatus;
+    private Project project;
+    private Priority priority;
+    private Type defectType;
 }
